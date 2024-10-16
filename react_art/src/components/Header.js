716bc38b038b8cb70c01e310d498d14ae699cd38
Jacob Christensen Admin <jChristensenAdmin@weimerbearing.com>
2024-10-16 09:47:44 -0500

    Added CSS modules and Styled-Components

--- react_art/src/components/Header.js
+++ react_art/src/components/Header.js
@@ -1,11 +1,19 @@
 import logo from '../assets/logo.png';
+import classes from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={`${classes.paragraph}`}
+        // style={{
+        //     // color: 'red',
+        //     // camelCase replaces the use of the '-' character in inline styles (text-align => textAlign)
+        //     // or you wrap the style key in quotes (text-align => 'text-align')
+        //     textAlign: 'center'
+        // }}
+      >A community of artists and art-lovers.</p>
     </header>
   );
 }
